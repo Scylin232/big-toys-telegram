0ddf01080f24d118b976ef3d49815864b6cb5b85
Scylin232 <timyr3@ukr.net>
2020-02-16 17:01:57 +0300

    Settings

--- src/services/Crontab/index.ts
+++ src/services/Crontab/index.ts
@@ -1,6 +1,7 @@
 import schedule from 'node-schedule'
 import axios from 'axios'
 import dotenv from 'dotenv'
+import { settingsModel } from '../MongoDB'
 
 let easyPayData = { pageId: null, appId: null, bearerToken: null }
 let counter = 0
@@ -8,18 +9,14 @@ let counter = 0
 dotenv.config()
 
 const scheduler = () => {
-  axios.get('http://0.tcp.ngrok.io:13309/').then(res => {
-    easyPayData = res.data
-    counter += 1
-    console.log('Completed: ', counter)
+  schedule.scheduleJob('*/10 * * * *', async () => {
+    const easyPayUrl = await settingsModel.findById('5e4946e00bf3af40d4150070')
+    axios.get(easyPayUrl.value).then(res => {
+      easyPayData = res.data
+      counter += 1
+      console.log('Completed: ', counter)
+    })
   })
-  // schedule.scheduleJob('*/10 * * * *', () => {
-  //   axios.get('http://0.tcp.ngrok.io:13309/').then(res => {
-  //     easyPayData = res.data
-  //     counter += 1
-  //     console.log('Completed: ', counter)
-  //   })
-  // })
 }
 
 export { scheduler, easyPayData }
